1bef901dd8dbc81068083516d2745069a74cf786
Luis Mayta <luis@hadenlabs.com>
2019-12-20 21:04:43 -0500

    refactor: make async for scmbreeze (#5)

--- zsh-scmbreeze.zsh
+++ zsh-scmbreeze.zsh
@@ -8,8 +8,6 @@
 #   Luis Mayta <slovacus@gmail.com>
 #
 
-LIGHT_GREEN='\033[1;32m'
-CLEAR='\033[0m'
 
 PATH_SCM_BREEZE=$HOME/.scm_breeze
 
@@ -21,15 +19,17 @@ df_command_exists() (
     command -v "$1" >/dev/null 2>&1
 )
 
-
 function scmbreeze::install {
-    echo -e "${CLEAR}${LIGHT_GREEN}Installing SCMBREEZE${CLEAR}"
-    git clone git://github.com/ndbroadbent/scm_breeze.git ${PATH_SCM_BREEZE}
-    ${PATH_SCM_BREEZE}/install.sh
+    if ! type -p git > /dev/null; then
+        message_info "not found git, please install git"
+    fi
+    message_info "Installing SCMBREEZE"
+    git clone git://github.com/ndbroadbent/scm_breeze.git "${PATH_SCM_BREEZE}"
+    "${PATH_SCM_BREEZE}/install.sh"
     zstyle ':notify:*' success-title "Finished install dependences"
 }
 
-function scmbreeze::run {
+function scmbreeze::install::async {
     if df_command_exists async_start_worker; then
         async_init
         # Start a worker that will report job completion
@@ -38,6 +38,12 @@ function scmbreeze::run {
         async_register_callback scmbreeze_worker_install scmbreeze::completed::callback
         # Start the job
         async_job scmbreeze_worker_install scmbreeze::install
+    fi
+}
+
+function scmbreeze::install::factory {
+    if df_command_exists async_start_worker; then
+        scm_breeze::install::async
     else
         scmbreeze::install
     fi
@@ -50,11 +56,12 @@ function scmbreeze::completed::callback {
 
 function scmbreeze::init {
     # Add SCMBREEZE to PATH for scripting
-    [ -s "$HOME/.scm_breeze/scm_breeze.sh" ] && source "$HOME/.scm_breeze/scm_breeze.sh"
+    # shellcheck source=/dev/null
+    [ -s "${HOME}/.scm_breeze/scm_breeze.sh" ] && source "${HOME}/.scm_breeze/scm_breeze.sh"
 }
 
 scmbreeze::init
 
-if [[ ! -e $PATH_SCM_BREEZE ]]; then
-    scmbreeze::run
+if [ ! -e "${PATH_SCM_BREEZE}" ]; then
+    scmbreeze::install::factory
 fi
